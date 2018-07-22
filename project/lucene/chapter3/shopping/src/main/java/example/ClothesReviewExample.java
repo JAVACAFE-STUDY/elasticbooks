c787a852dfa00b44016d49de717afa89dd49d1c8
heejung.hwang <gyahoo617@gmail.com>
2018-07-22 22:04:15 +0900

    3장 예제 위치 이동

---
+++ project/lucene/chapter3/shopping/src/main/java/example/ClothesReviewExample.java
@@ -0,0 +1,30 @@
+package example;
+
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import service.IndexService;
+import service.SearchService;
+import util.CsvLoader;
+import vo.CustomerReview;
+
+import java.util.List;
+
+public class ClothesReviewExample {
+    public static void main(String args[]) {
+        //CSV 파일로 부터 데이터를 읽어온다.
+        List<CustomerReview> reviewList = CsvLoader.readReview();
+
+        //Directory를 사용한다.
+        Directory index = new RAMDirectory();
+
+        IndexService indexService = new IndexService();
+        indexService.indexCustomerReview(index, reviewList);
+
+        //분석 결과를 확인한다.
+        SearchService searchService = new SearchService();
+        String userQuery = "shirt";
+        String fieldName = "reviewText";
+        int maxHitCount = 10;
+        searchService.serach(index, fieldName, userQuery, maxHitCount);
+    }
+}
