923a4ac377e6c0e0ec38eaf4170b80b4f73144e7
pjh0410 <pjh0410@gmail.com>
2018-09-24 02:23:07 +0900

    movie-finder skel 추가

---
+++ project/elasticSearch/chapter2/movie-finder/src/main/java/io/javacafe/elasticbooks/moviefinder/FirebaseSampleApplication.java
@@ -0,0 +1,12 @@
+package io.javacafe.elasticbooks.moviefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseSampleApplication.class, args);
+	}
+}
